409e3caec6c9ff01d0b8173d3a1606d5d1ec2d97
tchiikun <tchiikun@hotmail.fr>
2013-11-20 17:37:00 +0100

    TutoXNA

---
+++ GTE/GTE/GameMain.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace GTE
+{
+    class GameMain
+    {
+        // FIELDS
+        Rectangle Player;
+
+        // CONSTRUCTOR
+        public GameMain()
+        {
+            Player = new Rectangle(0, 0, 16, 16);
+        }
+
+        // METHODS
+
+        // UPDTA & DRAW
+        public void Updtate(MouseState mouse, KeyboardState keyboard)
+        {
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Resources.Player, Player, Color.LimeGreen);
+        }
+    }
+}
---
+++ GTE/GTE/Resources.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GTE
+{
+    class Resources
+    {
+        // STATIC FIELDS
+        public static Texture2D Player;
+
+        // LOAD CONTENT
+        public static void LoadContent(ContentManager Content)
+        {
+            Player = Content.Load<Texture2D>("player");
+        }
+    }
+}
