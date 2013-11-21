77472f4a2418e206ba84e1a07997ccaca8848e4f
tchiikun <tchiikun@hotmail.fr>
2013-11-21 04:45:32 +0100

    Try

--- GTE/GTE/Resources.cs
+++ GTE/GTE/Resources.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
 
 namespace GTE
 {
@@ -12,6 +13,8 @@ namespace GTE
     {
         // STATIC FIELDS
         public static Texture2D Player, Pikachu, Pixel;
+        public static SpriteFont Font1;
+        public static SoundEffect Match, Travailler;
 
         // LOAD CONTENT
         public static void LoadContent(ContentManager Content)
@@ -19,6 +22,9 @@ namespace GTE
             Player = Content.Load<Texture2D>("player");
             Pikachu = Content.Load<Texture2D>("Pikachu");
             Pixel = Content.Load<Texture2D>("pixel");
+            Font1 = Content.Load<SpriteFont>("Font1");
+            Match = Content.Load<SoundEffect>("match_ready_no_focus");
+            //Travailler = Content.Load<SoundEffect>("ttc");
         }// Dans cette fonction on load les textures.
     }
 }
