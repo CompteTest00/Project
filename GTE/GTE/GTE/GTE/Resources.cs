9d5e3a6db938ff229f28d6896adaa66a96ec856e
TheTastyWaffle <thomas.josso@gmail.com>
2014-03-10 16:07:04 +0100

    Update Resources.cs

--- GTE/GTE/GTE/GTE/Resources.cs
+++ GTE/GTE/GTE/GTE/Resources.cs
@@ -11,6 +11,8 @@ namespace GTE
     {
         //STATIC FIELDS
         public static Texture2D texture_player, texture_pointer, texture_bullet, texture_enemy, texture_blood, texture_bloodground;
+        /* map textures */
+        public static Texture2D texture_window1, texture_window2, texture_wood, texture_wood_down, texture_wood_right, texture_wood_left, texture_wood_up; 
         //RESOURCES
         public static void LoadContent(ContentManager Content)
         {
@@ -19,6 +21,13 @@ namespace GTE
             texture_bullet = Content.Load<Texture2D>("temporarybulletsprite");
             texture_enemy = Content.Load<Texture2D>("Enemy_temp");
             texture_blood = Content.Load<Texture2D>("blood");
+            texture_window1 = Content.Load<Texture2D>("MapSprites//window1");
+            texture_window2 = Content.Load<Texture2D>("MapSprites//window2");
+            texture_wood = Content.Load<Texture2D>("MapSPrites//wood");
+            texture_wood_down = Content.Load<Texture2D>("MapSprites//woodWdown");
+            texture_wood_left = Content.Load<Texture2D>("MapSprites//woodWleft");
+            texture_wood_right = Content.Load<Texture2D>("MapSprites//woodWright");
+            texture_wood_up = Content.Load<Texture2D>("MapSprites//woodWup");
         }
     }
 }
